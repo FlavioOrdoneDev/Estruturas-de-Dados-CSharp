88d752743cda69543cf490e4af15a695b74a2915
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-10-23 15:54:28 -0300

    Adicionado secao Filas

---
+++ Filas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Filas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+
+            queue.Enqueue(5);
+            queue.Enqueue(3);
+            queue.Enqueue(7);
+            queue.Enqueue(1);
+
+            queue.PrintQueue();
+            Console.WriteLine();
+
+            Console.WriteLine("Valor retirado: " + queue.Dequeue());
+
+            queue.PrintQueue();
+            Console.WriteLine();
+
+            queue.Enqueue(8);
+            queue.PrintQueue();
+            Console.WriteLine();
+
+
+            Console.WriteLine("O primeiro da fila é: " + queue.Peek());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Filas/Queue.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filas
+{
+    public class Queue
+    {
+        CNo ancora;
+        CNo trabalho;
+        public Queue()
+        {
+            ancora = new CNo();
+            ancora.Proximo = null;
+        }
+
+        public void PrintQueue()
+        {
+            trabalho = ancora;
+
+            while (trabalho.Proximo != null)
+            {
+                trabalho = trabalho.Proximo;
+                int dado = trabalho.Dado;
+                Console.Write(" {0}<-", dado);
+            }
+
+            Console.WriteLine();
+        }
+
+        public void Enqueue(int dado)
+        {
+            trabalho = ancora;
+
+            while (trabalho.Proximo != null)
+            {
+                trabalho = trabalho.Proximo;
+            }
+
+            CNo temp = new CNo();
+
+            temp.Dado = dado;
+            temp.Proximo = null;
+
+            trabalho.Proximo = temp;
+        }
+
+        public int Dequeue()
+        {
+            int valor = 0;
+
+            if (ancora.Proximo != null)
+            {
+                trabalho = ancora.Proximo;
+                valor = trabalho.Dado;
+
+                ancora.Proximo = trabalho.Proximo;
+                trabalho.Proximo = null;
+            }
+
+            return valor;
+        }
+
+        public int Peek()
+        {
+            int valor = 0;
+
+            if (ancora.Proximo != null)
+            {
+                trabalho = ancora.Proximo;
+                valor = trabalho.Dado;                
+            }
+
+            return valor;
+        }
+    }
+}
