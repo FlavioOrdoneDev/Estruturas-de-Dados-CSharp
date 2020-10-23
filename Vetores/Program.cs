325df8592a16caaf1ac9bfdfd27eb7fa298b8c92
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-10-23 20:50:15 -0300

    Adiconado estudos sobre vetores

---
+++ Vetores/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vetor vetor = new Vetor(10);
+
+            vetor.Adiciona("elemento 1");
+            vetor.Adiciona("elemento 2");
+            vetor.Adiciona("elemento 3");
+            vetor.Adiciona("elemento 4");
+
+            Console.WriteLine(vetor.Busca(3));
+            
+            Console.WriteLine(vetor.BuscarElemento("elemento 1"));
+
+            vetor.MostraValores();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Vetores/Vetor.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vetores
+{
+    public class Vetor
+    {
+        public Vetor(int capacidade)
+        {
+            this.elementos = new string[capacidade];
+            this.tamanho = 0;
+        }
+
+        private string[] elementos;
+
+        private int tamanho;
+
+        public string Busca(int posicao)
+        {
+            if (posicao >= 0 && posicao < tamanho)
+                return this.elementos[posicao];
+            else
+                return "Posicao inválida";
+        }
+
+        public bool BuscarElemento(string elemento)
+        {
+            return this.elementos.Contains(elemento);
+        }
+
+        public bool Adiciona(string elemento)
+        {
+            if (this.tamanho < this.elementos.Length)
+            {
+                this.elementos[this.tamanho] = elemento;
+                this.tamanho++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public int QuantidadeElementos()
+        {
+            int total = 0;
+            foreach (var item in this.elementos)
+            {
+                if (item != null)
+                    total++;
+            }
+
+            return total;
+        }
+
+        public int Tamanho()
+        {
+            return this.tamanho;
+        }
+
+        public void MostraValores()
+        {
+            foreach (var item in this.elementos)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        //public override string ToString()
+        //{
+        //    string resultado = "";
+        //    foreach (var item in this.elementos)
+        //    {
+        //        resultado += string.Format("[{0}]", item);
+        //    }
+
+        //    return resultado; 
+        //}
+
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder();
+            s.Append("[");
+
+            for (int i = 0; i < this.tamanho -1; i++)
+            {
+                s.Append(this.elementos[i]);
+                s.Append(", ");
+            }
+
+            if (this.tamanho > 0)
+            {
+                s.Append(this.elementos[this.tamanho - 1]);
+            }
+
+            s.Append("]");
+            return s.ToString();
+        }
+    }
+}
